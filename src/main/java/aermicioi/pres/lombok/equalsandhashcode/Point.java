250732aa25825f719f944c056a3c6052db926ac7
Andrei Ermicioi <andrei.ermicioi@gmail.com>
2020-05-03 23:57:54 +0200

    #1-1-0002 - Added equals() and hashCode() methods

--- src/main/java/aermicioi/pres/lombok/equalsandhashcode/Point.java
+++ src/main/java/aermicioi/pres/lombok/equalsandhashcode/Point.java
@@ -1,5 +1,7 @@
 package aermicioi.pres.lombok.equalsandhashcode;
 
+import java.util.Objects;
+
 public class Point {
 
     private final int x;
@@ -8,5 +10,24 @@ public class Point {
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.x, this.y);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || !obj.getClass().equals(this.getClass())) {
+            return false;
+        }
+
+        final Point that = (Point) obj;
+        return Objects.equals(this.x, that.x) && Objects.equals(this.y, that.y);
     }
 }
