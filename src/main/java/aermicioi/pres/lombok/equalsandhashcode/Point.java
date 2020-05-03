90833b02a562a8f5674fe0e50e56bc50649ea583
Andrei Ermicioi <andrei.ermicioi@gmail.com>
2020-05-04 00:08:30 +0200

    #1-1-0008 - Added getter for z property

--- src/main/java/aermicioi/pres/lombok/equalsandhashcode/Point.java
+++ src/main/java/aermicioi/pres/lombok/equalsandhashcode/Point.java
@@ -2,16 +2,20 @@ package aermicioi.pres.lombok.equalsandhashcode;
 
 import lombok.EqualsAndHashCode;
 
-@EqualsAndHashCode(of = {"x", "y", "z"})
+@EqualsAndHashCode
 public class Point {
 
     private final int x;
     private final int y;
-    private final transient int z;
+    private final int z;
 
     public Point(int x, int y, int z) {
         this.x = x;
         this.y = y;
         this.z = z;
+    }
+
+    public int getZ() {
+        return 0;
     }
 }
